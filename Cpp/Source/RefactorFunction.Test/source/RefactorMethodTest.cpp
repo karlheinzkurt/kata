510aca7f9fe700d15ee9924bf4a64317ca3365f7
Sascha Lumma <s.lumma@gmail.com>
2016-04-17 12:21:25 +0200

    Initial infrastructure setup

---
+++ Cpp/Source/RefactorFunction.Test/source/RefactorMethodTest.cpp
@@ -0,0 +1,19 @@
+
+#include "RefactorFunction.Lib/include/Sophisticated.h"
+
+#include <gtest/gtest.h>
+
+TEST( First, Refactor )
+{
+    EXPECT_EQ(1, 1);
+}
+
+TEST( Second, Refactor )
+{
+    EXPECT_TRUE(true);
+}
+
+TEST( Third, Refactor )
+{
+    EXPECT_FALSE(false);
+}
---
+++ Cpp/coverage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+BUILD_DIR=$DIR/Build/Coverage
+BIN_DIR=$DIR/Bin
+SOURCE_DIR=$DIR/Source
+
+if [ ! -d "$BUILD_DIR" ]; then
+   mkdir -p $BUILD_DIR
+   pushd $BUILD_DIR
+      cmake -DCMAKE_BUILD_TYPE=Coverage $SOURCE_DIR
+   popd
+fi
+
+pushd $BUILD_DIR
+   make -B
+popd
+
+find $BIN_DIR -maxdepth 1 -name "*.Test" -exec {} \;
+gcovr \
+   -e ".+Test\.cpp"\
+   -e ".+main.cpp"\
+   -e ".+gmock/.+"\
+   -r $DIR/../\
+   --object-directory=$BUILD_DIR\
+   > $BUILD_DIR/coverage.txt
+cat $BUILD_DIR/coverage.txt
