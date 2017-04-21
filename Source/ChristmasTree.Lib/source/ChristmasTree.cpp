b952d85bda8cf3c1fb92c9297640cb7eaee43e28
Sascha Lumma <s.lumma@gmail.com>
2017-04-22 00:21:14 +0200

    Simplified calculation

--- Source/ChristmasTree.Lib/source/ChristmasTree.cpp
+++ Source/ChristmasTree.Lib/source/ChristmasTree.cpp
@@ -3,20 +3,15 @@
 
 #include <iostream>
 
-bool IsInside( unsigned int wide, unsigned int line, unsigned int column )
-{   
-   unsigned int const center( wide / 2 );
-   return ( column > ( center - line - 1 ) && column < ( center + line + 1 ) );  
-}
-
 void PrintChristmasTree( std::ostream& os, unsigned int height )
 {
    unsigned int const wide( height + ( height - 1 ) + 2 );
+   unsigned int const center( wide / 2 );
    for ( unsigned int line( 0 ); line < height; ++line )
    {
       for ( unsigned int column( 0 ); column < wide; ++column )
       {
-         os << ( IsInside( wide, line, column ) ? '*' : ' ' );
+         os << ( ( column > ( center - line - 1 ) && column < ( center + line + 1 ) ) ? '*' : ' ' );
       }
       os << '\n';
    }
